4f7cfa880c16705e1366947303f1cd7259b168d6
teamoteh <lim1473@gmail.com>
2022-02-13 21:22:41 +0800

    initial

--- pages/about.js
+++ pages/about.js
@@ -1,35 +1,29 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import Footer from "../comps/Footer";
-import Navbar from "../comps/Navbar";
-import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum odio
           consequatur nihil neque quam, at suscipit libero vero commodi nisi
           consectetur temporibus quae molestiae, perferendis tenetur harum iure
           possimus atque?
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum odio
           consequatur nihil neque quam, at suscipit libero vero commodi nisi
           consectetur temporibus quae molestiae, perferendis tenetur harum iure
           possimus atque?
         </p>
-        <Link href="/ninjas">
-          <a className={styles.btn}>See Ninja Listing</a>
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
