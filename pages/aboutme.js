c0449f1ec9232d20359caad0bd10fc597083351e
teamoteh <lim1473@gmail.com>
2022-02-27 21:08:29 +0800

    finished up aboutme page

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -4,8 +4,6 @@ import Image from "next/image";
 import styles from "../styles/h1.module.css";
 import PostImage from "./Picture";
 
-
-
 const About = () => {
   return (
     <>
@@ -13,24 +11,25 @@ const About = () => {
         <title>About Timothy Lim</title>
         <meta name="keywords" content="ninjas" />
       </Head>
-      
+
       <div>
         <h1>About Me</h1>
-        
-          <div className={styles.image}>
-            <Image className="image-wrapper"
-            src ="/photo6111707233957031763.jpg"
-            alt = "Picture of Me!"
+
+        <div className={styles.image}>
+          <Image
+            className="image-wrapper"
+            src="/photo6111707233957031763.jpg"
+            alt="Picture of Me!"
             width={1000}
             height={800}
-            />
-          </div>
-        
+          />
+        </div>
+
         <div className={styles.typewrite}>
           <Typewriter
             options={{
-            loop: true,
-            deleteSpeed: 2,
+              loop: true,
+              deleteSpeed: 2,
             }}
             onInit={(typewriter) => {
               typewriter
@@ -46,17 +45,45 @@ const About = () => {
             }}
           />
         </div>
-        <p className = {styles.para}>
-          I am currently a Year 2 student in Nanyang Technological University and 
-          pursuing a bachelor in Computer Science. As a driven individual, I take
-          it into my stride to pursue knowledge in areas that aren't covered in my
-          school. I am open to opportunities that allow me to expand my horizons and
-          provide me with new experiences to enjoy and environments for me to learn 
-          and grow. Let's connect!
+        <p className={styles.para}>
+          I am currently a Year 2 student in Nanyang Technological University
+          and pursuing a bachelor in Computer Science. As a driven individual, I
+          take it into my stride to pursue knowledge in areas that aren't
+          covered in my school. I am open to opportunities that allow me to
+          expand my horizons and provide me with new experiences to enjoy and
+          environments for me to learn and grow. Let's connect!
         </p>
       </div>
       <div>
-      <h2>dad very fat</h2>
+        <hr />
+        <h2 className={styles.h2}>coursework</h2>
+        <ul>
+          <li>Languages: Python, Java, Javascript, C/C++, SQL</li>
+          <li>Frameworks: React, Next, NODE</li>
+          <li>
+            Modules: Algorithms, Object-Oriented Design, Data Structures,
+            Software Engineering, Full Stack Developent, Operating Systems, Data
+            Analytics, Artificial Intelligience
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <h2 className={styles.h2}>clubs & societies</h2>
+        <ul>
+          <li>A' Division Saint Angrew's Rugby Team </li>
+          <li> President of NTU Touch Rugby Men's</li>
+          <li>Captain of Hall's Touch Rugby</li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <h2 className={styles.h2}>awards & accomplishments</h2>
+        <ul>
+          <li>MOE Academic Excellence Award</li>
+          <li>Finalist MLDA Hackathon 2021</li>
+          <li>National Colours Award</li>
+        </ul>
       </div>
     </>
   );
