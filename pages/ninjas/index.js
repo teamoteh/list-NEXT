dc46e55f2aba41eb491a68c982a84f5345d4babc
teamoteh <lim1473@gmail.com>
2022-03-02 15:59:11 +0800

    for vercel

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import styles from "../../styles/Ninjas.module.css";
-import blogPostWithImage from "./blogCard";
-import React from 'react';
+import React from "react";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -16,9 +15,7 @@ const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>Projects</h1>
-      <div>
-      blogPostWithImage()
-    </div>
+      <div>blogPostWithImage()</div>
       {ninjas.map((ninja) => (
         <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a className={styles.single}>
@@ -27,7 +24,6 @@ const Ninjas = ({ ninjas }) => {
         </Link>
       ))}
     </div>
-    
   );
 };
 
